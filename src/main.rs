135933512a689abfd201258f79981229a421bf9e
Arnav-Bista <arnav.bista12@gmail.com>
2023-05-14 12:49:18 +0530

    parsing + starting the wfc

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
 use std::env;
+use rand::Rng;
 mod tile;
 
-const DEFAULT_DIR: &str = "./";
+const DEFAULT_DIR: &str = "./assets/config/";
 
 pub fn main() {
     // let args: Vec<String> = env::args().collect();
+    let data = tile::Tile::get_tile_list(DEFAULT_DIR)
+        .expect("Error loading configurations.");
+    let board = initialise(data, 10);
+    println!("{:#?}",board);
 
-    let data = tile::Tile::get_tile_list(DEFAULT_DIR);
-    println!("{:#?}",data);
+}
+
+fn initialise(data: Vec<tile::Tile>, size: u32) -> Vec<Vec<Vec<tile::Tile>>> {
+    let mut board: Vec<Vec<Vec<tile::Tile>>> = Vec::new();
+
+    for i in 0..size {
+        board.push(Vec::new());
+        for j in 0..size {
+            board[i as usize].push(Vec::new());
+            for tile in &data {
+                let mut tile = tile.clone();
+                for k in 0..4 {
+                    tile.set_current_rotation(k);
+                    board[i as usize][j as usize].push(tile.clone());
+                }
+            }
+        }
+    }
+
+    board
 }
--- src/tile.rs
+++ src/tile.rs
@@ -2,31 +2,33 @@ use serde::Deserialize;
 use serde_json::Result;
 use std::fs;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct Tile {
     name: String,
     socket_id: u8,
     sockets: Vec<u8>,
-    prototypes: Vec<Prototype>
+    #[serde(skip_deserializing)]
+    current_rotation: u8,
+    src: Vec<String>,
 }
 
-#[derive(Deserialize, Debug)]
-struct Prototype {
-    rotation: u8,
-    src: String
-}
 
 impl Tile {
     pub fn get_tile_list(path_to_dir: &str) -> Result<Vec<Tile>> {
         let mut result : Vec<Tile> = Vec::new();
         let paths = fs::read_dir(path_to_dir).unwrap();
         for path in paths {
-            // println!("Name: {}", path.unwrap().path().display())
-            let file_json = fs::read_to_string(path.unwrap().path())
-            .expect("Could not open file.");
-            let tile: Tile = serde_json::from_str(&file_json)?;
+            // println!("Name: {}", path.unwrap().path().display());
+            let file_json = fs::read_to_string(path.as_ref().unwrap().path())
+                .expect("Could not open file.");
+            let tile: Tile = serde_json::from_str(&file_json)
+                .unwrap_or_else(|_| panic!("Error parsing '{}' check if the path is set up properly.", path.unwrap().path().display()));
             result.push(tile);
         }
         Ok(result)
+    }
+
+    pub fn set_current_rotation(&mut self, rotation: u8) {
+        self.current_rotation = rotation;
     }
 }
